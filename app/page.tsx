64d8119330a6e91bae2d7e269b21fd6f648fcdb5
CarlosAcosta44 <carlosacosta12007@gmail.com>
2026-03-18 11:40:11 -0500

    feat(blog): add BlogSection server component mapping blog posts

--- app/page.tsx
+++ app/page.tsx
@@ -3,12 +3,16 @@
 
 import HeroSection from "@/components/HeroSection";
 import { heroImage } from "@/lib/data";
+import BlogSection from "@/components/BlogSection"
+import Gallery from "@/components/Gallery";
 
 export default function HomePage() {
   return (
     <>
       <HeroSection imageSrc={heroImage.src} imageAlt={heroImage.alt} />
       {/* Las demás secciones se agregarán en features posteriores */}
+      <Gallery/>
+      <BlogSection />
     </>
   );
 }
